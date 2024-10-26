9d5712c2a31ad605a47a15e3347223508f97f0b9
Pranjal Kushwaha <pranjal0819@gmail.com>
2024-10-26 14:20:19 +0530

    Setup JWT Guard Gem

---
+++ lib/jwt_guard.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "jwt_guard/version"
+
+module JwtGuard
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/jwt_guard_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe JwtGuard do
+  it "has a version number" do
+    expect(JwtGuard::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
